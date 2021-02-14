ed1723dc75fc1fb3c59beed63cb8e1a4498af9a6
jkim000 <joshuakim514@gmail.com>
2021-02-14 15:54:53 -0800

    updating main

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const savedMovieController = require('../controllers/savedMovieController');
+const userListController = require('../controllers/userListController');
+const { route } = require('../server');
 
 
 
@@ -25,6 +27,12 @@ router.get('/select/:imdbid',
 // router.update();
 
 // router.delete();
+
+
+route.get('/usersaved', userListController.userSaved, (req, res) => {
+  console.log(res.locals.userSavedList);
+  res.status(200).json(res.locals.userSavedList);
+})
 
 
 module.exports = router;
