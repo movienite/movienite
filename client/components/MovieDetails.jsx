291588522f797badd1c481d8ba8b176e8920e594
Jenny Hai <jennyhai@jennys-mbp.lan>
2021-02-15 11:17:16 -0800

    debug searchbar

--- client/components/MovieDetails.jsx
+++ client/components/MovieDetails.jsx
@@ -50,62 +50,62 @@ class MoviesDetails extends Component {
 
   componentDidMount() {
     //FETCH REQUEST TO BACKEND
-    // const id = this.props.match.params.id;
-    // fetch(`/api/select/${id}`, {
-    //   method: 'GET',
-    //   header: {
-    //     'Content-Type': 'application/json; charset="UTF-8"',
-    //   }
-    //  })
-    //   .then((data) => data.json())
-    //   .then(data => {
-    //     this.setState(state => data)
-    //   })
+    const id = this.props.match.params.id;
+    fetch(`/api/select/${id}`, {
+      method: 'GET',
+      header: {
+        'Content-Type': 'application/json; charset="UTF-8"',
+      }
+     })
+      .then((data) => data.json())
+      .then(data => {
+        this.setState(state => data)
+      })
 
     //DUMMY DATA: 
-    const data = {
-      "Title": "Inception",
-      "Year": "2010",
-      "Rated": "PG-13",
-      "Released": "16 Jul 2010",
-      "Runtime": "148 min",
-      "Genre": "Action, Adventure, Sci-Fi, Thriller",
-      "Director": "Christopher Nolan",
-      "Writer": "Christopher Nolan",
-      "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
-      "Plot": "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
-      "Language": "English, Japanese, French",
-      "Country": "USA, UK",
-      "Awards": "Won 4 Oscars. Another 152 wins & 218 nominations.",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-      "Ratings": [
-          {
-              "Source": "Internet Movie Database",
-              "Value": "8.8/10"
-          },
-          {
-              "Source": "Rotten Tomatoes",
-              "Value": "87%"
-          },
-          {
-              "Source": "Metacritic",
-              "Value": "74/100"
-          }
-      ],
-      "Metascore": "74",
-      "imdbRating": "8.8",
-      "imdbVotes": "2,058,501",
-      "imdbID": "tt1375666",
-      "Type": "movie",
-      "DVD": "N/A",
-      "BoxOffice": "$292,576,195",
-      "Production": "Syncopy, Warner Bros.",
-      "Website": "N/A",
-      "Response": "True",
-      "trailer": "youtu.be/Jvurpf91omw"
-    }
+    // const data = {
+    //   "Title": "Inception",
+    //   "Year": "2010",
+    //   "Rated": "PG-13",
+    //   "Released": "16 Jul 2010",
+    //   "Runtime": "148 min",
+    //   "Genre": "Action, Adventure, Sci-Fi, Thriller",
+    //   "Director": "Christopher Nolan",
+    //   "Writer": "Christopher Nolan",
+    //   "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
+    //   "Plot": "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+    //   "Language": "English, Japanese, French",
+    //   "Country": "USA, UK",
+    //   "Awards": "Won 4 Oscars. Another 152 wins & 218 nominations.",
+    //   "Poster": "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    //   "Ratings": [
+    //       {
+    //           "Source": "Internet Movie Database",
+    //           "Value": "8.8/10"
+    //       },
+    //       {
+    //           "Source": "Rotten Tomatoes",
+    //           "Value": "87%"
+    //       },
+    //       {
+    //           "Source": "Metacritic",
+    //           "Value": "74/100"
+    //       }
+    //   ],
+    //   "Metascore": "74",
+    //   "imdbRating": "8.8",
+    //   "imdbVotes": "2,058,501",
+    //   "imdbID": "tt1375666",
+    //   "Type": "movie",
+    //   "DVD": "N/A",
+    //   "BoxOffice": "$292,576,195",
+    //   "Production": "Syncopy, Warner Bros.",
+    //   "Website": "N/A",
+    //   "Response": "True",
+    //   "trailer": "youtu.be/Jvurpf91omw"
+    // }
 
-    this.setState(state => data);
+    // this.setState(state => data);
 
   }
 
