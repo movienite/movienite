b55aa5194e7ed6a1b51504bc570a5a3d2dd70022
jkim000 <joshuakim514@gmail.com>
2021-02-15 19:13:49 -0800

    delete line 4 require for deleted userListController file

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const savedMovieController = require('../controllers/savedMovieController');
-const userListController = require('../controllers/userListController');
 const { route } = require('../server');
 
 
